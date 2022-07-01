db9e1b8ee572be108dec51f52762dc0be1063c19
Pedro Marques da Silva <posilva@gmail.com>
2022-07-01 14:13:20 +0100

    Fix version of elixir to be supported on Ubuntu

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -13,7 +13,7 @@ defmodule Webtest.MixProject do
 
       app: :webtest,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
